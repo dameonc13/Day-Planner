327ebc745b73e140e5a44fa1c2dc9171af49658a
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-01-05 21:44:40 -0500

    Cleaned up JS file

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,14 @@
-//var today = new Date();
-//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-//var dateTime = date+' '+time;
+
 var container = $(".container")
 var currentTime = new moment().format("h A");
-$("#currentDay").text(currentTime)
+var currentDate = new moment().format("MM-DD-YYYY")
+$("#currentDay").text( currentDate)
+$("header").append( currentTime)
 
 
-//console.log(dateTime);
-//console.log(moment());
-
-//const n =moment().add(2, "hours"); 
 
 
-//moment().subtract(7, 'hours');
-
-//m.set("hour", 9)
+// appends the hours to the Times array  using a for loop
 Times = [];
 for (var i = 9; i < 18; i++) {
     i += 0;
@@ -27,7 +20,7 @@ for (var i = 9; i < 18; i++) {
 
 
 
-// Thetimes = [];
+// for loop used to create the timeblocks for the Day Planner
 for (var i = 0; i < Times.length; i++) {
     i += 0;
     var textarea = $("<textarea/>", {
@@ -56,10 +49,10 @@ for (var i = 0; i < Times.length; i++) {
     saveCol.addClass("fa fa-save")
     form.addClass("col col-md-12  ")
     saveCol.addClass("col col-md-2 saveBtn")
-    // saveCol.css( " border-bottom-right-radius: 15px");
-
+    
+// Apply  a specific color to each timeblock depending on what the current time is  
     if (Times[i]< currentTime ) {
-        console.log("yes") 
+        
         textarea.css("background-color", "grey") 
     }
     else if (Times[i] > currentTime){
@@ -69,6 +62,7 @@ for (var i = 0; i < Times.length; i++) {
         textarea.css("background-color", "red") 
     }
 
+
 }
 
 
@@ -77,73 +71,30 @@ for (var i = 0; i < Times.length; i++) {
 
 
 
-
-// our array
-var movies = ["Reservoir Dogs", "Pulp Fiction", "Jackie Brown",
-    "Kill Bill", "Death Proof", "Inglourious Basterds"];
-
-// storing our array as a string
-localStorage.setItem("quentinTarantino", JSON.stringify(movies));
-
-// retrieving our data and converting it back into an array
-var retrievedData = localStorage.getItem("quentinTarantino");
-var movies2 = JSON.parse(retrievedData);
-
-//making sure it still is an array
-console.log(movies2.length);
+// takes user input and saves it to local storage
 
 $(".saveBtn").on("click", function (event) {
 
     event.preventDefault();
 
-    var activity = []
+    var activity = ''
 
     for (var i = 0; i < usrinput.length; i++) {
-        activity.push(usrinput[i].value)
+        activity = (usrinput[i].value)
 
-        localStorage.setItem("activity", JSON.stringify(activity));
+        localStorage.setItem("activity", activity );
     }
 });
 
-desc = []
-for (var i = 0; i < desc.length; i++) {
+tfrdata = []
+for (var i = 0; i < Times.length; i++) {
 
     var recvdata = (localStorage.getItem("activity "))
-    var tfrdata = JSON.parse(recvdata)
-    console.log(recvdata.length);
+     tfrdata.push(JSON.parse(recvdata))
+    
 
     infoCol.text[tfrdata[i]]
 }
-console.log(tfrdata);
+console.log( recvdata);
 
-/*console.log(Times);
-var m = moment().subtract(i, "hours")
-// m.hours(); // 5
-// m.add(1, 'days').hours(); // 5
-// m = moment(  "4:00AM" , " h:mmA")
-// m = moment.utc(i+":50PM" , " h:mmA");
 
-if (moment('2010-10-20').isBefore('2010-10-21') == false) {
-    console.log("yes");
-
-}
-else {
-    console.log("csd");
-
-}
-var Currenttime = ` ${m.toString()}`
-var CurrenttimeS = `toString() => ${m.toString()}`
-// console.log(`toString() => ${m.toString()}`);
-//console.log(`toISOString() => ${m.toISOString()}`);
-//console.log(Currenttime);
-//console.log(CurrenttimeS);
-
-console.log(m.format("h A"));
-//console.log(m.hour(), );
-
-v/*'8:45am', 'h:mma');
-$("header").append(beginningTime.toDate())
-var endTime = moment('9:00am', 'h:mma');
-console.log(beginningTime.isBefore(endTime)); // true
-); // Mon May 12 2014 08:45:00
-console.log(endTime.toDate()); // Mon May 12 2014 09:00:00   */
