6954a551a6a9d072669aec98a7bcc4922ad8bda7
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-01-06 22:05:54 -0500

    Cleaned up JS File making the Planner fully functional

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var container = $(".container")
 
 //Adds current date and time to the header
-var currentTime = new moment().format("HH A");
+var currentTime = new moment().format("HH mm A");
 var currentDate = new moment().format("MM-DD-YYYY")
 $("#currentDay").text( currentDate)
 $("header").append( currentTime)
@@ -26,7 +26,7 @@ for (var i = 9; i < 18; i++) {
 for (var i = 0; i < Times.length; i++) {
     i += 0;
     var textarea = $("<textarea/>", {
-        id: "usrinput",
+        id: "usrinput" + i,
         type: "text",
         rows: '4',
 
@@ -45,32 +45,26 @@ for (var i = 0; i < Times.length; i++) {
     container.append(div);
     textarea.addClass("form-control");
     form.addClass("form-group");
-    div.addClass("row justify-content-md-center");
+    div.addClass("row");
     timeCol.addClass("col col-md-2 hour ");
-    infoCol.addClass("col col-md-8  ");
+    infoCol.addClass("col col-md-9 time-block  ");
     saveCol.addClass("fa fa-save")
-    form.addClass("col col-md-12  ")
-    saveCol.addClass("col col-md-2 saveBtn")
+    saveCol.addClass("col col-md-1 saveBtn")
     
 // Apply  a specific color to each timeblock depending on what the current time is  
     if (Times[i] < currentTime) {
         
-        textarea.css("background-color", "grey") 
+        textarea.addClass("past") 
     }
     else if (Times[i] > currentTime){
-        textarea.css("background-color", "green") 
+        textarea.addClass("present")
     }
     else if (Times[i] == currentTime){
-        textarea.css("background-color", "red") 
+        textarea.addClass("future")
     }
 
 
 }
-
-
-
-
-
 
 
 // takes user input and saves it to local storage
@@ -79,26 +73,49 @@ $(".saveBtn").on("click", function (event) {
 
     event.preventDefault();
 
-    var activity = ''
+    localStorage.setItem("input0", usrinput0.value );
+    localStorage.setItem("input1", usrinput1.value );
+    localStorage.setItem("input2", usrinput2.value );
+    localStorage.setItem("input3", usrinput3.value );
+    localStorage.setItem("input4", usrinput4.value );
+    localStorage.setItem("input5", usrinput5.value );
+    localStorage.setItem("input6", usrinput6.value );
+    localStorage.setItem("input7", usrinput7.value );
+    localStorage.setItem("input8", usrinput8.value ); 
 
-    for (var i = 0; i < usrinput.length; i++) {
-        activity = (usrinput.value)
 
-        localStorage.setItem("activity", activity );
-    }
-
-console.log( activity);
 });
 
-tfrdata = []
-for (var i = 0; i < Times.length; i++) {
+// user input is  stores to a variable for each hour
 
-    var recvdata = (localStorage.getItem("activity "))
-     tfrdata.push(JSON.parse(recvdata))
+    var recvdata0 = localStorage.getItem("input0")
+    var recvdata1 = localStorage.getItem("input1")
+    var recvdata2 = localStorage.getItem("input2")
+    var recvdata3 = localStorage.getItem("input3")
+    var recvdata4 = localStorage.getItem("input4")
+    var recvdata5 = localStorage.getItem("input5")
+    var recvdata6 = localStorage.getItem("input6")
+    var recvdata7 = localStorage.getItem("input7")
+    var recvdata8 = localStorage.getItem("input8")
     
 
-    infoCol.text[tfrdata[i]]
-}
-console.log( recvdata);
+// appends text to time-blocks which stays there even when the page refreshes until save button is clicked again
+    $("#usrinput0").text(recvdata0)
+    $("#usrinput1").text(recvdata1)
+    $("#usrinput2").text(recvdata2)
+    $("#usrinput3").text(recvdata3)
+    $("#usrinput4").text(recvdata4)
+    $("#usrinput5").text(recvdata5)
+    $("#usrinput6").text(recvdata6)
+    $("#usrinput7").text(recvdata7)
+    $("#usrinput8").text(recvdata8)
+   
+      
+ 
+ 
+    
+
+   
+
 
 
